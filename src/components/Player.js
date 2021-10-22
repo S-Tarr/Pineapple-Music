e8c55c904604031d77ba0aaae9a0feb3c782dda4
Jason Paik <paik2@purdue.edu>
2021-10-22 16:46:48 -0400

    added play/pause funcitonality to the song page

--- src/components/Player.js
+++ src/components/Player.js
@@ -1,11 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Components.css";
+import song from "./testSong.wav"
 
 function Player() {
+    const [audio, setAudio] = useState(new Audio(song));
+    const [isPlaying, setState] = useState(false);
+
+    const playPause = () => {
+        if (isPlaying) {
+            audio.pause();
+            setState(false);
+        }
+        else {
+            audio.play();
+            setState(true);
+        }
+    };
+
     return(
         <div className="Player-Div">
             <button className="Player">Skip</button>
-            <button className="Player">Play/Pause</button>
+            <button className="Player" onClick={() => playPause()}>Play/Pause</button>
             <button className="Player">Rewind</button>
         </div>
     );
