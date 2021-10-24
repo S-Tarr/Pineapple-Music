f7c65cbb174de65ee2ac450c4eb03c13a3644835
S-Tarr <tarrs@purdue.edu>
2021-10-24 19:45:16 -0400

    initial firebase implementation and queue storage

--- src/pages/SearchPage.js
+++ src/pages/SearchPage.js
@@ -26,7 +26,7 @@ const handleSpotifyLogin = () => {
     window.location = `${SPOTIFY_AUTHORIZE_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URL_AFTER_LOGIN}&scope=${SCOPES_URL_PARAM}&response_type=token&show_dialog=true`;
   };
 
-
+  
 function SearchPage() {
     const [authorized, setAuthorized] = useState(true);
     const [token, setToken] = useState({})
