24027d6234dc0f8598f53833dbac79eab6387c40
S-Tarr <tarrs@purdue.edu>
2021-10-24 16:19:24 -0400

    fixed album image size

--- src/Components/SearchBar.js
+++ src/Components/SearchBar.js
@@ -34,19 +34,11 @@ function SearchBar({ placeholder, spotifyData, authorized }) {
         console.log(res.body.tracks.items);
         setSearchResults(
             res.body.tracks.items.map(track => {
-              const smallestAlbumImage = track.album.images.reduce(
-                (smallest, image) => {
-                  if (image.height < smallest.height) return image
-                  return smallest
-                },
-                track.album.images[0]
-              )
-    
               return {
                 artist: track.artists[0].name,
                 title: track.name,
                 uri: track.uri,
-                albumUrl: smallestAlbumImage.url,
+                albumUrl: track.album.images[0].url,
               }
             })
         )
